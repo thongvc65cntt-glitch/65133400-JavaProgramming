02332abc9bcb6b3cd7de6269a763c8f0558846c4
VoChiThong65133400 <thong.vc.65cntt@ntu.edu.vn>
2026-01-20 20:45:06 +0700

    Update XinChao.java

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -13,8 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tôi đang thử chạy Java, và thử nghiệm quản lý mã nguồn");
-
+		System.out.println("Tôi đang thử chạy JAVA, và thử nghiệm quản lý mã nguồn");
+		System.out.print("Tôi thêm một dòng mới và chỉnh lại chữ JAVA ở dòng trên");
 	}
 
 }
